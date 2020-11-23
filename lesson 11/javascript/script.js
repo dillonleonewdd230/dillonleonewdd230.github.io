79b784515f532dcad3341f8fa4eee91c621c83a7
dillonleonewdd230 <dillonleone@byui.edu>
2020-11-23 14:12:32 -0700

    fix

---
+++ lesson 11/javascript/script.js
@@ -0,0 +1,46 @@
+function toggleMenu(){
+    
+    document.getElementById("primaryNav").classList.toggle("hide")
+
+
+}
+const dow = 
+[ 
+'Sunday',
+'Monday',
+'Tuesday',
+'Wednesday',
+'Thurseday',
+'Friday',
+'Saturday'
+];
+const months= 
+[
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December' 
+];
+
+let date = new Date();
+let output = dow[date.getDay()] + ', ' + date.getDate() + ' ' + months[date.getMonth()] + ' '+ date.getFullYear();
+
+
+
+
+
+
+
+ document.getElementById('current-date').innerText = output;
+
+if (date.getDay()==5){
+    document.getElementById("banner").style.display="block";
+}
